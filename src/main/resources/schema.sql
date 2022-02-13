25d41b45de74bcfca93f1a0e6f59cfeb36897507
Vitaly Ko <vitaly@lcs.lv>
2022-02-13 19:13:25 +0200

    VAA-4 refactor code

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,7 +7,7 @@ USE airlines;
 DROP TABLE IF EXISTS airplanes;
 DROP TABLE IF EXISTS crews_crew_members;
 DROP TABLE IF EXISTS crews;
-DROP TABLE IF EXISTS crew_members;
+DROP TABLE IF EXISTS crew_member;
 
 CREATE TABLE crews
 (
@@ -23,11 +23,11 @@ CREATE TABLE airplanes
     manufacture_date DATE        NOT NULL,
     capacity         INT         NOT NULL,
     flight_range     INT         NOT NULL,
-    crews_id         INT,
-    CONSTRAINT FK_airplanes_crews FOREIGN KEY (crews_id) REFERENCES crews (id)
+    crew_id         INT,
+    CONSTRAINT FK_airplanes_crews FOREIGN KEY (crew_id) REFERENCES crews (id)
 );
 
-CREATE TABLE crew_members
+CREATE TABLE crew_member
 (
     id          INT AUTO_INCREMENT PRIMARY KEY,
     first_name  VARCHAR(75) NOT NULL,
@@ -43,6 +43,6 @@ CREATE TABLE crews_crew_members
     crew_id        INT NOT NULL,
     crew_members_id INT NOT NULL,
     PRIMARY KEY (crew_id, crew_members_id),
-    CONSTRAINT FK_crew_members_id FOREIGN KEY (crew_members_id) REFERENCES crew_members (id),
+    CONSTRAINT FK_crew_members_id FOREIGN KEY (crew_members_id) REFERENCES crew_member (id),
     CONSTRAINT FK_crews_crew_members FOREIGN KEY (crew_id) REFERENCES crews (id)
 );
